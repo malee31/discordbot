e87172549ce674d0449a129ff5045686acbe2c64
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-07-25 22:18:15 -0700

    Added basic commands. Wow this is neat

--- run.js
+++ run.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = ">";
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -7,8 +8,28 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 	console.log(msg);
-	if (msg.content === 'ping') {
-		msg.reply('pong');
+	if (msg.content.startsWith(prefix)) {
+		let args = msg.content.slice(prefix.length).trim().split(" ");
+		let command = args.shift().toLowerCase();
+		switch(command)
+		{
+			case "echo":
+				msg.reply(args.join(" "));
+			break;
+			case "dm":
+				msg.reply("Of course, on it");
+				msg.author.send("Here's the DM you asked for: \"" + args.join(" ") + "\"");
+				msg.author.send("Here's your information: " + JSON.stringify(msg.author));
+			break;
+			case "shutdown":
+				console.log("Shutdown requested by: " + msg.author.username + "#" + msg.author.discriminator);
+				msg.reply(":(").then(() => {
+					process.exit();
+				});
+			break;
+			default:
+				msg.reply("I have no idea what you are trying to say");
+		}
 	}
 });
 
