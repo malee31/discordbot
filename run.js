ca48db58e4d5f23d95f6a69e80cf2cf096e464c8
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-10-05 19:46:33 -0700

    SUPPORTING MULTIPLE COMMANDS IN ONE LINE NOW!

--- run.js
+++ run.js
@@ -14,39 +14,53 @@ client.on('message', msg => {
 	const hasEmbed = msg.embeds.length !== 0;
 	if(msg.author.bot || !(hasText || hasImage || hasEmbed) || !msg.content.startsWith(config.prefix)) return;
 
-	let args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
-	let command = config.aliases(args.shift().toLowerCase());
-	let joined = args.join(" ");
-	switch(command)
-	{
-		case "greet":
-		case "shutdown":
-		case "tree":
-		case "profile":
-			commands[command](msg);
-		break;
-		case "say":
-		case "echo":
-		case "dm":
-		case "dmall":
-			commands[command](msg, joined);
-		break;
-		case "baretree":
-			commands.tree(msg, true);
-		break;
-		case "regex":
-			commands.regex(msg, args[0]);
-		break;
-		case "spam":
-			commands.spam(msg, args);
-		break;
-		case "test":
-			console.log(joined.split("\n"));
-		case "celebrate":
-			commands.say(msg, "Yay!!! I worked properly!!!");
-		break;
-		default:
-			msg.reply("I have no idea what you are trying to say");
+	let content = msg.content.slice(config.prefix.length).trim();
+
+	let subcommands = content.split(/\|/g);
+	
+	for(let commandNum = 0; commandNum < subcommands.length; commandNum++) {
+		let processCommand = subcommands[commandNum].trim().split(/ |\n+/g);
+		subcommands[commandNum] = {
+			command: config.aliases(processCommand.shift().toLowerCase()),
+			args: processCommand.join(" ").trim()
+		};
+	}
+
+	for(let runningCommand = 0; runningCommand < subcommands.length; runningCommand++) {
+		let command = subcommands[runningCommand].command;
+		let joined = subcommands[runningCommand].args;
+		let args = subcommands[runningCommand].args.split(" ");
+		switch(command)
+		{
+			case "greet":
+			case "shutdown":
+			case "tree":
+			case "profile":
+				commands[command](msg);
+			break;
+			case "say":
+			case "echo":
+			case "dm":
+			case "dmall":
+				commands[command](msg, joined);
+			break;
+			case "baretree":
+				commands.tree(msg, true);
+			break;
+			case "regex":
+				commands.regex(msg, args[0]);
+			break;
+			case "spam":
+				commands.spam(msg, args);
+			break;
+			case "test":
+				console.log(joined.match(/'([^']+)'/g));
+			case "celebrate":
+				commands.say(msg, "Yay!!! I worked properly!!!");
+			break;
+			default:
+				msg.reply("I have no idea what you are trying to say");
+		}
 	}
 });
 
